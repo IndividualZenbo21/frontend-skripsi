69d2758ee704df161b68db86cf8e0b8e0eed9a29
IndividualZenbo21 <93858454+IndividualZenbo21@users.noreply.github.com>
2024-03-11 14:46:33 +0700

    Router fix (#18)
    * add route
    
    * fix route

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routes';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
+import ProductPage from './pages/products';
+import ErrorPage from './pages/404';
 
 const BASE_PATH = '/frontend-skripsi';
 
@@ -10,6 +14,10 @@ const router = createBrowserRouter([
   { path: `${BASE_PATH}/`, 
     element: <div>Hello World!</div> 
   },
+  { path: `${BASE_PATH}/login`, element: <LoginPage /> },
+  { path: `${BASE_PATH}/register`, element: <RegisterPage /> },
+  { path: `${BASE_PATH}/products`, element: <ProductPage /> },
+  { path: '*', element: <ErrorPage/> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(

ae2679c2ec1f648f29dc10afa1e7c1dbc56d89fc
william <williamhalim712@gmail.com>
2024-03-11 16:54:01 +0700

    add value

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -44,11 +44,11 @@ const FormLogin = () => {
       <>
       <div className="mb-6">
             <label className="block text-slate-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-            <input className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" label="email" type="email" placeholder="example@mail.com" name="email" id="email" onChange={setHandleFormChange('email')} disabled={loading}/>
+            <input className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" label="email" type="email" placeholder="example@mail.com" onChange={setHandleFormChange('email')} value={formState.email} disabled={loading}/>
           </div>
           <div className="mb-6">
             <label className="block text-slate-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
-            <input className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" label="password" type="password" placeholder="*****" name="password" id="password" onChange={setHandleFormChange('email')} disabled={loading}/>
+            <input className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" label="password" type="password" placeholder="*****" onChange={setHandleFormChange('email')} value={formState.password} disabled={loading}/>
           </div>
           <button
             className="h-10 px-6 font-semibold rounded-md bg-blue-600 text-white"

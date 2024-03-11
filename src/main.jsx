5f9fa1f5f1db3a81fecf6d8ccd155a09a66d7714
william <williamhalim712@gmail.com>
2024-03-11 16:08:59 +0700

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import { createBrowserRouter, HashRouter, RouterProvider } from 'react-router-dom';
-import { routes } from './routes';
 import LoginPage from './pages/login';
 import RegisterPage from './pages/register';
 import ProductPage from './pages/products';
@@ -10,18 +9,20 @@ import ErrorPage from './pages/404';
 
 const BASE_PATH = '/frontend-skripsi';
 
-const router = createBrowserRouter([
-  { path: `${BASE_PATH}/`, 
-    element: <LoginPage />,
-  },
-  { path: `${BASE_PATH}/login`, element: <LoginPage /> },
-  { path: `${BASE_PATH}/register`, element: <RegisterPage /> },
-  { path: `${BASE_PATH}/products`, element: <ProductPage /> },
-  { path: '*', element: <ErrorPage/> },
-]);
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path={`${BASE_PATH}/`} element={<LoginPage />} />
+      <Route path={`${BASE_PATH}/login`} element={<LoginPage />} />
+      <Route path={`${BASE_PATH}/register`} element={<RegisterPage />} />
+      <Route path={`${BASE_PATH}/products`} element={<ProductPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </Router>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter router={router}></HashRouter>
+    <App />
   </React.StrictMode>
 );

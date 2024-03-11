58cb316feb9b541be31a70046896d77d2ae44aa5
Individual_Zenbo21 <jonathanowenkawinda@gmail.com>
2024-03-11 12:52:58 +0700

    fix

--- src/components/Elements/Input/Input.jsx
+++
@@ -1,8 +0,0 @@
-const Input = (props) => {
-    const {type, placeholder, name, onChange, value} = props || {}
-    return (
-        <input type={type} className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50"  placeholder={placeholder} name={name} id={name} onChange={onChange} value={value}/>
-    );
-}
-
-export default Input

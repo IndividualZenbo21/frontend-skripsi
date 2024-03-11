b36c00d49790bd982dd9027b58562ee92a1187c8
william <williamhalim712@gmail.com>
2024-03-11 13:52:00 +0700

    [William] add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import LoginPage from './pages/login.jsx'
-import RegisterPage from './pages/register.jsx'
-import ErrorPage from './pages/404.jsx'
-import ProductPage from './pages/products.jsx'
-
-const route = [
-  {
-    path:"/",
-    element: <div>Hello World</div>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path:"/login",
-    element: <LoginPage/>,
-  },
-  {
-    path:"/register",
-    element: <RegisterPage/>
-  },
-  {
-    path:"/products",
-    element: <ProductPage/>
-  }
-]
-
-const router = createBrowserRouter (route)
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Router>
+      <Switch>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} exact={route.exact}>
+            <route.component />
+          </Route>
+        ))}
+      </Switch>
+    </Router>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);

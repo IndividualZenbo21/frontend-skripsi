8dc51e3d0d4a1ad87e0d43486e4a11a9c0e0f9fa
william <williamhalim712@gmail.com>
2024-03-11 14:24:04 +0700

    [William] fix path

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
 
+const BASE_PATH = '/frontend-skripsi';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename={BASE_PATH}>
       <Routes>
         {routes.map((route) => (
           <Route key={route.path} path={route.path} exact={route.exact} element={<route.component />} /> 
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,12 @@ import RegisterPage from './pages/register.jsx'
 import ErrorPage from './pages/404.jsx'
 import ProductPage from './pages/products.jsx'
 
+const BASE_PATH = '/frontend-skripsi';
+
 export const routes = [
-    { path: '/', component: () => <div>Hello World</div>, exact: true },
-    { path: '/login', component: LoginPage, exact: true },
-    { path: '/register', component: RegisterPage, exact: true },
-    { path: '/products', component: ProductPage, exact: true },
-    { path: '*', component: ErrorPage },
-  ];
+  { path: `${BASE_PATH}/`, component: LoginPage, exact: true },
+  { path: `${BASE_PATH}/login`, component: LoginPage },
+  { path: `${BASE_PATH}/register`, component: RegisterPage },
+  { path: `${BASE_PATH}/products`, component: ProductPage },
+  { path: '*', component: ErrorPage },
+];

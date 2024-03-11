2a1dfdb32f00a7c85e6b5f62cb7f597799d63d32
Individual_Zenbo21 <jonathanowenkawinda@gmail.com>
2024-03-11 13:40:10 +0700

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {createBrowserRouter, RouterProvider, RouteObject} from "react-router-dom"
 import LoginPage from './pages/login.jsx'
 import RegisterPage from './pages/register.jsx'
 import ErrorPage from './pages/404.jsx'
 import ProductPage from './pages/products.jsx'
 
-const router = createBrowserRouter ([
+const route = [
   {
     path:"/",
     element: <div>Hello World</div>,
@@ -25,7 +25,9 @@ const router = createBrowserRouter ([
     path:"/products",
     element: <ProductPage/>
   }
-])
+]
+
+const router = createBrowserRouter (route)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

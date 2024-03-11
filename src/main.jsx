7a3fb7198b1cf3ab84289254dd09d4d64fefcb1e
Individual_Zenbo21 <jonathanowenkawinda@gmail.com>
2024-03-11 14:37:27 +0700

    add route

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routes';
 
 const BASE_PATH = '/frontend-skripsi';
 
+const router = createBrowserRouter([
+  { path: `${BASE_PATH}/`, 
+    element: <div>Hello World!</div> 
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename={BASE_PATH}>
-      <Routes>
-        {routes.map((route) => (
-          <Route key={route.path} path={route.path} exact={route.exact} element={<route.component />} /> 
-        ))}
-      </Routes>
-    </Router>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );

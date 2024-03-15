6d0b4e1939c14e94588917d8835a90bacd939c0e
william <williamhalim712@gmail.com>
2024-03-15 23:56:59 +0700

    add login button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import './HomePage.css';
+import config from "../components/Config/default.json";
 
 function HomePage() {
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        navigate(`${config.baseUrl}/login`);
+    };
+
     return (
         <div className="home">
             <h1>Welcome to User Level Access Manager</h1>
             <p>Manage access levels efficiently and effectively.</p>
+            <button className="login-button" onClick={handleLoginClick}>Login</button>
         </div>
     );
 }

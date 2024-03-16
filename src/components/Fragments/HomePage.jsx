4b90e69285496dff80b46c84815da733aa10a530
Individual_Zenbo21 <jonathanowenkawinda@gmail.com>
2024-03-16 20:28:31 +0700

    test navbar

--- src/components/Fragments/HomePage.jsx
+++ src/components/Fragments/HomePage.jsx
@@ -1,6 +1,7 @@
 import {useNavigate} from "react-router-dom";
 import config from "../Config/default.json";
 import './styles/HomePage.css';
+import { Fragment } from "react";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -10,11 +11,14 @@ const HomePage = () => {
   };
 
   return (
+    <Fragment>
+      <Navbar/>
       <div className="home">
         <h1>Welcome to User Level Access Manager</h1>
         <p>Manage access levels efficiently and effectively.</p>
         <button className="home-login-button" onClick={handleLoginClick}>Login</button>
       </div>
+    </Fragment>
   );
 
 }
